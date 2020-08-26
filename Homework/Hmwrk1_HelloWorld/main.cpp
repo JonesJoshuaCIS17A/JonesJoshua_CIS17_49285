8669c52b23f3e4d2b59ea16c0c13bd6c582e369a
JonesJoshuaCIS17A <jjones172@student.rccd.edu>
2020-08-26 15:10:17 -0700

    Signed-off-by: JonesJoshuaCIS17A <jjones172@student.rccd.edu>

---
+++ Homework/Hmwrk1_HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+/*Programmer:   Joshua Jones
+ Assignment: Homework 1_HelloWorld
+ Filename: Hmwrk1_HelloWorld.cpp
+ Creation Date: 8-26-2020
+ Description:   This program outputs the line "Hello World"
+ */
+
+#include<iostream> //This line of code causes the contents of another file
+                    //to be inserted to the program
+
+using namespace std;
+
+//The following line of code begins every C++ program.
+int main(int argc, char** argv) {
+    cout<<"Hello world."<<endl;
+
+    return 0;
+}
+
